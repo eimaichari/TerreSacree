49aa32ccfd0ca720d56c15d69d3675c6f4f2d3e7
eimaichari <kutigrace9@gmail.com>
2025-06-09 16:46:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+  let slides = document.querySelectorAll(".slide");
+  let index = 0;
+  function showNextSlide() {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+  }
+  setInterval(showNextSlide, 5000);
